f386aebecb1ba3130d191ed2b702fd82d3e4f6d8
Mina Elia Louize <107637818+MinaEliaa@users.noreply.github.com>
2023-03-03 12:02:32 +0200

    using retrofit package with Gson converter Get http request from newsapi.org (server)

---
+++ app/src/main/java/com/example/newsapp/api/model/SourcesItem.kt
@@ -0,0 +1,11 @@
+package com.example.newsapp.api.model
+
+data class SourcesItem(
+	val country: String? = null,
+	val name: String? = null,
+	val description: String? = null,
+	val language: String? = null,
+	val id: String? = null,
+	val category: String? = null,
+	val url: String? = null
+)
---
+++ app/src/main/java/com/example/newsapp/api/model/SourcesResponse.kt
@@ -0,0 +1,6 @@
+package com.example.newsapp.api.model
+
+data class SourcesResponse(
+	val sources: List<SourcesItem?>? = null,
+	val status: String? = null
+)
